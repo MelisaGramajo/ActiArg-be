439fe66a255d3421c0cfa2eadf58bdf939759b88
MelisaGramajo <melisargramajo@gmail.com>
2020-07-21 20:11:48 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -67,7 +67,7 @@ router.put('/updateCarousel/:id',AuthMdw.ensureAuthenticated, CarouselController
 router.post('/addTurn',AuthMdw.ensureAuthenticated, TurnController.add);
 router.get('/Turns',AuthMdw.ensureAuthenticated, TurnController.search);
 router.get('/TurnbyId/:idDay',AuthMdw.ensureAuthenticated, TurnController.searchById);
-router.post('/reserveTurn/:idDay',AuthMdw.ensureAuthenticated, TurnController.reserve);
+router.post('/reserveTurn/:idDay', TurnController.reserve);
 router.put('/deleteTurn/:id',AuthMdw.ensureAuthenticated, TurnController.delete);
 
 //Mercado Pago
